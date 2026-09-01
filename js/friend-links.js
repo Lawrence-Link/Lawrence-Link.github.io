6427d30f16bbeb3b6f39174d170576c828a9ecc4
Lawrence <68999458+Lawrence-Link@users.noreply.github.com>
2026-09-01 17:40:27 +0800

    Site updated: YYYY-09-Sep 1, 2026 17:40:27

--- js/friend-links.js
+++ js/friend-links.js
@@ -8,7 +8,7 @@
     { name: 'wildcreator', url: 'https://wildcreator.top/', icon: '/img/friendslink/wildcreator.jpg' },
     { name: 'x1193', url: 'http://x1193.wikidot.com', icon: '/img/friendslink/X1193.png' },
     { name: '渣渣120', url: 'https://zhazha120.cn', icon: '/img/friendslink/zhazha120.png' },
-    { name: '天明', url: 'https://www.xming.cloud', icon: '/img/friendslink/tming.png'},
+    { name: '明宇', url: 'https://www.xming.cloud', icon: '/img/friendslink/tming.png'},
     { name: '绀漓の锟斤拷', url: 'https://blog.sevtinge.com/friends', icon: '/img/friendslink/gl.png'}
   ]
 
